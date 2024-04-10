887b1c87317ebb15d046ba053793fa1717f57c13
Madeleine Hartland-Dykes <146767175+hd2802@users.noreply.github.com>
2024-04-10 15:25:05 +0100

    fixes duplication bug

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import requests
 import time
 from bs4 import BeautifulSoup
 from fix import fix
+from merger import merger
 from urllib.parse import urljoin
 
 target_url = "https://people.bath.ac.uk/masrjb/CourseNotes/cm20318.html"
 
 slides = []
+fixed_slides = []
 
 def download_slides(url):
     response = requests.get(url)
@@ -25,22 +27,26 @@ def download_slides(url):
             i += 1
             print("Downloading file #" + str(i))
             filename = os.path.join(folder_location, link['href'].split('/')[-1])
-            fix_filename = os.path.join(folder_location, filename.replace(".pdf", "_fix.pdf"))
+            fix_filename = os.path.join(folder_location, os.path.splitext(os.path.basename(link['href']))[0] + '_fix.pdf')
 
+            fixed_slides.append(fix_filename)
+            
             # If the file already exists or if the fixed version already exists, no download is required
             # Currently does not work! - fix!
-            if not os.path.isfile(filename) or not os.path.isfile(fix_filename):
+            if not os.path.isfile(filename) and not os.path.isfile(fix_filename):
                 slides.append(filename)
                 with open(filename, 'wb') as f:
                     f.write(requests.get(urljoin(url, link['href'])).content)
+            else:
+                print("Download not required")
 
 def fix_slides():
-    #i = 0
+    i = 0
     for slide in slides:
         if os.path.isfile(slide):
             # Printing this causes the program to run slower!
-            #i += 1
-            #print("Fixing file #" + str(i))
+            i += 1
+            print("Fixing file #" + str(i))
             fix(slide)
             os.remove(slide)
 
@@ -48,5 +54,6 @@ if __name__ == "__main__":
     start = time.time()
     download_slides(target_url)
     fix_slides()
+    merger(slides)
     end = time.time()
     print("Execution time: ", end - start, " seconds")
